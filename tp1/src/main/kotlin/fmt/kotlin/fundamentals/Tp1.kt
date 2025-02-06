3520356edca2545a9c8ddfb8d8ad2e9bee5cd9b1
ctruchi <cyril.truchi@4sh.fr>
2025-02-06 09:51:23 +0400

    tp1

---
+++ tp1/src/main/kotlin/fmt/kotlin/fundamentals/Tp1.kt
@@ -0,0 +1,42 @@
+package fmt.kotlin.fundamentals
+
+var x = 0
+
+fun incrementXAndReturnOldValue(): Int {
+    return -1
+}
+
+fun incrementXAndReturnNewValue(): Int {
+    return -1
+}
+
+fun sumZeroPointFive(): Float {
+    return -1f
+}
+
+fun subtractBigNumber(): Int {
+    return -1
+}
+
+fun buildRangeOpen(): IntRange {
+    return 0..0
+}
+
+fun buildRangeOpenEnded(): IntRange {
+    return 0..0
+}
+
+/**
+ * n Number of bottles. Always more than 2.
+ */
+fun describeNbBottles(): String {
+    return ""
+}
+
+var totalBottles = 0
+var nbWhiteBottles = 0
+var nbRedBottles = 0
+
+fun describeWithDetailNbBottles(): String {
+    return ""
+}
