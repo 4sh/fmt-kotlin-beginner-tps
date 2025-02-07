ab54fd997e1aa62513b9bad1462c268f6a224b24
ctruchi <cyril.truchi@4sh.fr>
2025-02-07 16:08:46 +0400

    tp3 step 2

--- tp3/src/test/kotlin/fmt/kotlin/fundamentals/CellarTest.kt
+++ tp3/src/test/kotlin/fmt/kotlin/fundamentals/CellarTest.kt
@@ -3,7 +3,6 @@ package fmt.kotlin.fundamentals
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
-import strikt.api.expectThrows
 import strikt.assertions.isEqualTo
 
 class CellarTest {
@@ -34,6 +33,41 @@ class CellarTest {
         @Test
         fun `should remove 20 barrels`() {
             val cellar = Cellar(generateBarrels(100))
+
+            cellar.removeBarrels(20)
+
+            expectThat(cellar) {
+                get { totalBarrels }.isEqualTo(80)
+            }
+        }
+    }
+
+    @Nested
+    inner class SecondaryConstructor {
+
+        @Test
+        fun `should initialize barrels with 100 barrels`() {
+            val cellar = Cellar(100)
+
+            expectThat(cellar) {
+                get { totalBarrels }.isEqualTo(100)
+            }
+        }
+
+        @Test
+        fun `should add 50 barrels`() {
+            val cellar = Cellar(100)
+
+            cellar.addBarrels(50)
+
+            expectThat(cellar) {
+                get { totalBarrels }.isEqualTo(150)
+            }
+        }
+
+        @Test
+        fun `should remove 20 barrels`() {
+            val cellar = Cellar(100)
 
             cellar.removeBarrels(20)
 
