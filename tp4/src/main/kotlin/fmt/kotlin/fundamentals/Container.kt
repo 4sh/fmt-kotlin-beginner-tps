7c3f69a30accf0186b21531e850cebb787bbe7e6
ctruchi <cyril.truchi@4sh.fr>
2025-02-10 11:53:07 +0400

    tp4 step 4 solution

--- tp4/src/main/kotlin/fmt/kotlin/fundamentals/Container.kt
+++ tp4/src/main/kotlin/fmt/kotlin/fundamentals/Container.kt
@@ -1,12 +1,23 @@
 package fmt.kotlin.fundamentals
 
-abstract class Container(
+sealed class Container(
     val capacity: Int,
     possibleCapacities: IntRange
 ) {
     val valid = (possibleCapacities).contains(capacity)
 
-    fun containersNeededToPourIn(container: Container) = -1
+    fun containersNeededToPourIn(container: Container) = when (this) {
+        is Tank -> when (container) {
+            is Barrel, is FixedVolumeContainer -> capacity / container.capacity
+            is Tank -> -1
+        }
+
+        is Barrel -> if (container is FixedVolumeContainer) {
+            capacity / container.capacity
+        } else -1
+
+        is FixedVolumeContainer -> -1
+    }
 }
 
 class Barrel(
@@ -17,7 +28,7 @@ class Tank(
     capacity: Int
 ) : Container(capacity, 2000000..10000000)
 
-abstract class FixedVolumeContainer(capacity: Int) : Container(capacity, capacity..capacity)
+sealed class FixedVolumeContainer(capacity: Int) : Container(capacity, capacity..capacity)
 
 class Magnum : FixedVolumeContainer(150)
 class Bottle : FixedVolumeContainer(75)
