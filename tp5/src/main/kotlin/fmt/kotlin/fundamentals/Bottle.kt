5e1419a3443b6971021aa5be81cfa3c9d079dc60
ctruchi <cyril.truchi@4sh.fr>
2025-02-12 11:06:19 +0400

    tp5 step 1

---
+++ tp5/src/main/kotlin/fmt/kotlin/fundamentals/Bottle.kt
@@ -0,0 +1,11 @@
+package fmt.kotlin.fundamentals
+
+class Bottle(
+    val name: String,
+    val year: Int
+) {
+
+    fun isEqualTo(bottle: Bottle) = false
+
+    fun isSameInstance(bottle: Bottle) = false
+}
