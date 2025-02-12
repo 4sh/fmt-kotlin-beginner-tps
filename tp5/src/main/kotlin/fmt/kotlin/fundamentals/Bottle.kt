bd85a507151d293e956fa160f31609c91364ca7c
ctruchi <cyril.truchi@4sh.fr>
2025-02-12 11:16:06 +0400

    tp5 step 2 solution

--- tp5/src/main/kotlin/fmt/kotlin/fundamentals/Bottle.kt
+++ tp5/src/main/kotlin/fmt/kotlin/fundamentals/Bottle.kt
@@ -2,10 +2,23 @@ package fmt.kotlin.fundamentals
 
 data class Bottle(
     val name: String,
-    val year: Int
+    val year: Int,
+    val color: WineColor
 ) {
 
     fun isEqualTo(bottle: Bottle) = this == bottle
 
     fun isSameInstance(bottle: Bottle) = this === bottle
+
+    override fun toString() = "Bouteille de $name ${color.translate()} de $year"
+}
+
+enum class WineColor {
+    RED,
+    WHITE;
+
+    fun translate() = when (this) {
+        RED -> "rouge"
+        WHITE -> "blanc"
+    }
 }
