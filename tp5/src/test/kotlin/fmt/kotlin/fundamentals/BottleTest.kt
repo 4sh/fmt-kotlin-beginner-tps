a97bbb7d9a189246b00b8c6427283aff3ce2f1f6
ctruchi <cyril.truchi@4sh.fr>
2025-02-12 11:15:54 +0400

    tp5 step 2

--- tp5/src/test/kotlin/fmt/kotlin/fundamentals/BottleTest.kt
+++ tp5/src/test/kotlin/fmt/kotlin/fundamentals/BottleTest.kt
@@ -1,5 +1,7 @@
 package fmt.kotlin.fundamentals
 
+import fmt.kotlin.fundamentals.WineColor.RED
+import fmt.kotlin.fundamentals.WineColor.WHITE
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Nested
 import strikt.api.expectThat
@@ -14,38 +16,56 @@ class BottleTest {
     inner class Equality {
         @Test
         fun `should be equals if name and year are equals`() {
-            val isEqual = Bottle("Coucheroy", 2005).isEqualTo(Bottle("Coucheroy", 2005))
+            val isEqual = Bottle("Coucheroy", 2005, RED).isEqualTo(Bottle("Coucheroy", 2005, RED))
 
             expectThat(isEqual).isTrue()
         }
 
         @Test
         fun `should not be equals if year is different`() {
-            val isEqual = Bottle("Coucheroy", 2005).isEqualTo(Bottle("Coucheroy", 2009))
+            val isEqual = Bottle("Coucheroy", 2005, RED).isEqualTo(Bottle("Coucheroy", 2009, RED))
 
             expectThat(isEqual).isFalse()
         }
 
         @Test
         fun `should not be equals if name is different`() {
-            val isEqual = Bottle("Coucheroy", 2005).isEqualTo(Bottle("Pedesclaux", 2009))
+            val isEqual = Bottle("Coucheroy", 2005, RED).isEqualTo(Bottle("Pedesclaux", 2009, RED))
 
             expectThat(isEqual).isFalse()
         }
 
         @Test
         fun `should not be same instance even if name and year are equals`() {
-            val isEqual = Bottle("Coucheroy", 2005).isSameInstance(Bottle("Coucheroy", 2005))
+            val isEqual = Bottle("Coucheroy", 2005, RED).isSameInstance(Bottle("Coucheroy", 2005, RED))
 
             expectThat(isEqual).isFalse()
         }
 
         @Test
         fun `should be same instance`() {
-            val bottle = Bottle("Coucheroy", 2005)
+            val bottle = Bottle("Coucheroy", 2005, RED)
             val isEqual = bottle.isSameInstance(bottle)
 
             expectThat(isEqual).isTrue()
+        }
+    }
+
+    @Nested
+    inner class ToString {
+
+        @Test
+        fun `should describe the bottle of red`() {
+            val s = Bottle("Coucheroy", 2005, RED).toString()
+
+            expectThat(s).isEqualTo("Bouteille de Coucheroy rouge de 2005")
+        }
+
+        @Test
+        fun `should describe the bottle of white`() {
+            val s = Bottle("Yquem", 2006, WHITE).toString()
+
+            expectThat(s).isEqualTo("Bouteille de Yquem blanc de 2006")
         }
     }
 }
