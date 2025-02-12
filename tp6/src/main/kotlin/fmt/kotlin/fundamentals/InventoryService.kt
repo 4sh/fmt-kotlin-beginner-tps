8ed97a20e574a950cafc7455a865b5b5d81a4b66
ctruchi <cyril.truchi@4sh.fr>
2025-02-12 15:48:23 +0400

    tp6 step 3 solution

--- tp6/src/main/kotlin/fmt/kotlin/fundamentals/InventoryService.kt
+++ tp6/src/main/kotlin/fmt/kotlin/fundamentals/InventoryService.kt
@@ -1,7 +1,18 @@
 package fmt.kotlin.fundamentals
 
 class InventoryService {
+
     val bottles = mutableListOf<Bottle>()
 
     val errors = mutableListOf<String>()
+
+    fun addAll(bottlesText: List<String>) {
+        for (s in bottlesText) {
+            s.toBottle()?.add() ?: errors.add(s)
+        }
+    }
+
+    fun Bottle.add() {
+        bottles.add(this)
+    }
 }
