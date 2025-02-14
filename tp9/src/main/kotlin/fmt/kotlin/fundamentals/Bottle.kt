b3a759fc84bbb62b0ec7e94a32b4e8931a43a226
ctruchi <cyril.truchi@4sh.fr>
2025-02-14 11:08:00 +0400

    tp9 step 2

--- tp9/src/main/kotlin/fmt/kotlin/fundamentals/Bottle.kt
+++ tp9/src/main/kotlin/fmt/kotlin/fundamentals/Bottle.kt
@@ -2,5 +2,11 @@ package fmt.kotlin.fundamentals
 
 data class Bottle(
     val name: String,
-    val year: Int
+    val year: Int,
+    val color: WineColor
 )
+
+enum class WineColor {
+    RED,
+    WHITE
+}
