8172aed4f3fbdedc7a1812ac15dd639c335127dc
ctruchi <cyril.truchi@4sh.fr>
2025-02-14 10:48:08 +0400

    tp9 step 1 solution

--- tp9/src/main/kotlin/fmt/kotlin/fundamentals/Cellar.kt
+++ tp9/src/main/kotlin/fmt/kotlin/fundamentals/Cellar.kt
@@ -3,5 +3,9 @@ package fmt.kotlin.fundamentals
 data class Cellar(
     val bottles: List<Bottle>
 ) {
-    fun describeBottles() = ""
+    fun describeBottles() = bottles.joinToString("\n") { bottle ->
+        bottle.run {
+            "Bouteille de ${name} de ${year}"
+        }
+    }
 }
