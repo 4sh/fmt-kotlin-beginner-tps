2de71cdc2ee43d5430e0e738cc8949fbfa628748
ctruchi <cyril.truchi@4sh.fr>
2025-02-14 08:14:46 +0400

    tp8 step 1 solution

--- tp8/src/main/kotlin/fmt/kotlin/fundamentals/Tp8.kt
+++ tp8/src/main/kotlin/fmt/kotlin/fundamentals/Tp8.kt
@@ -2,5 +2,12 @@ package fmt.kotlin.fundamentals
 
 class Tp8 {
 
-    fun getFirstPrimeNumbers(nbToFind: Int) = emptyList()
+    fun getFirstPrimeNumbers(nbToFind: Int) = generateSequence(2) { it + 1 }
+        .filter { it.isPrime() }
+        .take(nbToFind)
+        .toList()
+
+    private fun Int.isPrime() = generateSequence(2) { it + 1 }
+        .takeWhile { it < this }
+        .none() { this % it == 0 }
 }
