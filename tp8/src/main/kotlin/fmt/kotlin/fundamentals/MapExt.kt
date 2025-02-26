bb4f1feb284701d5d0a7e1c87de61a166dd32755
ctruchi <cyril.truchi@4sh.fr>
2025-02-26 15:04:30 +0400

    tp8 step 3

--- tp8/src/main/kotlin/fmt/kotlin/fundamentals/MapExt.kt
+++ tp8/src/main/kotlin/fmt/kotlin/fundamentals/MapExt.kt
@@ -5,17 +5,8 @@ package fmt.kotlin.fundamentals
  *
  * If one map have no entry for a key, operation is not called and the existing value is used.
  *
- * @param K the type of keys in the maps
- * @param V the type of values in the maps
  * @param map       the map to merge with
  * @param operation an operation to call on entry with a value in both map to compute the new value
  * @return a new map with keys of both maps and values computed with [operation]
  */
-fun <K, V> Map<K, V>.merge(map: Map<K, V>, operation: (V, V) -> V): Map<K, V> =
-    (keys + map.keys).associateWith { key ->
-        if (this[key] != null && map[key] != null) {
-            operation(this[key]!!, map[key]!!)
-        } else {
-            this[key] ?: map[key]!!
-        }
-    }
+fun Map<String, Int>.merge(map: Map<String, Int>, operation: (Int, Int) -> Int): Map<String, Int> = TODO()
