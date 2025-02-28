6ae6bf8ad8d7711352a17319e54c3c9677e3f88f
paul.bourseau <paul.bourseau@4sh.fr>
2025-02-28 10:39:12 +0100

    tp12 step 2

--- tp12/src/test/kotlin/fmt/kotlin/fundamentals/CellarTest.kt
+++ tp12/src/test/kotlin/fmt/kotlin/fundamentals/CellarTest.kt
@@ -1,55 +1,51 @@
 package fmt.kotlin.fundamentals
 
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.test.assertSame
 
 class CellarTest {
-    @Test
-    fun `Should add n bottle`() {
-        //Etant donné que
-        val n = 5
-        val cellar = Cellar(n)
-
-        //Lorsque
-        for (i in 0..n) {
-            cellar.addBottle(Bottle("Latour", 1990, WineColor.RED))
-        }
-
-        //Alors
-        assertEquals(n, cellar.bottleCount)
-    }
 
     @Test
-    fun `Should not add more bottle than cellar space`() {
-        //Etant donné que
-        val n = 5
-        val cellar = Cellar(n - 2)
-
-        //Lorsque
-        for (i in 0..n) {
-            cellar.addBottle(Bottle("Latour", 1990, WineColor.RED))
-        }
-
-        //Alors
-        assertEquals(cellar.size, cellar.bottleCount)
-    }
-
-    @Test
-    fun `Should take first colored bottle`() {
+    fun `Should describe bottles`() {
         //Etant donné que
         val cellar = Cellar(5)
-        val bottleToRetrieve = Bottle("Margaux", 2012, WineColor.WHITE)
+
+        val bottleMock = //TODO
+
+        //TODO Mocker l'appel de Bottle.description pour retourner Bottle à chaque fois
+
+        cellar.addBottle(bottleMock)
+        cellar.addBottle(bottleMock)
+        cellar.addBottle(bottleMock)
+
+        //Lorsque
+        val description = cellar.describeBottles()
+
+        //Alors
+        assertEquals("Bottle\nBottle\nBottle", description)
+    }
+
+    @Test
+    fun `Should compute cellar value`() {
+        //Etant donné que
+        val wineStoreMock = //TODO
+        val cellar = Cellar(5)
+
+        //TODO Mocker l'appel de WineStoreMock.bottlePrice en capturant le paramètre
 
         cellar.addBottle(Bottle("Haut-Brion", 1997, WineColor.RED))
-        cellar.addBottle(bottleToRetrieve)
+        cellar.addBottle(Bottle("Margaux", 2012, WineColor.WHITE))
         cellar.addBottle(Bottle("Pétrus", 2008, WineColor.RED))
 
         //Lorsque
-        val bottle = cellar.takeColoredBottle(WineColor.WHITE)
+        val value = cellar.cellarValue()
 
         //Alors
-        assertEquals(2, cellar.bottleCount)
-        assertSame(bottle, bottleToRetrieve)
+        assertEquals(50, value)
+        //TODO Vérifier que le prix a été calculé trois fois (1 fois par bouteille)
     }
 }
