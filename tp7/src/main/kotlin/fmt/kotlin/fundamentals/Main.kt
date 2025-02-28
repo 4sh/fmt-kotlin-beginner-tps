22513d6acf7659f80fb4cd979aaf8aa24b035222
Élise Rubio <elise.rubio@4sh.fr>
2025-02-28 15:53:20 +0100

    tp 7 step 1 solution

--- tp7/src/main/kotlin/fmt/kotlin/fundamentals/Main.kt
+++ tp7/src/main/kotlin/fmt/kotlin/fundamentals/Main.kt
@@ -1,4 +1,27 @@
 package fmt.kotlin.fundamentals
 
+val printWineBottle: () -> Unit = { println("Wine bottle") }
+val shortlyDescribeWineBottleUsingExplicitAttributeName: (WineBottle) -> Unit = { wineBottle -> println(wineBottle.wineType)}
+val shortlyDescribeWineBottleUsingIt: (WineBottle) -> Unit = {  println(it.wineType)}
+val buildWineBottleDescription: (WineBottle) -> String = {
+    val bottleType = when(it.wineType) {
+        WineType.RED -> "rouge"
+        WineType.WHITE -> "blanc"
+        WineType.ROSE -> "rosé"
+    }
+
+    "Vin $bottleType de l'année ${it.year}"
+}
+
 fun main() {
+    printWineBottle()
+    val wineBottle = WineBottle(
+        WineType.RED,
+        2016,
+        "Isabelle",
+        1.0
+    )
+    shortlyDescribeWineBottleUsingExplicitAttributeName(wineBottle)
+    shortlyDescribeWineBottleUsingIt(wineBottle)
+    println(buildWineBottleDescription(wineBottle))
 }
