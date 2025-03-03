f51fe7ff4cdd321e4382ba903d320134106c7191
Élise Rubio <elise.rubio@4sh.fr>
2025-03-03 11:59:23 +0100

    tp7 step 3 solution

--- tp7/src/main/kotlin/fmt/kotlin/fundamentals/Main.kt
+++ tp7/src/main/kotlin/fmt/kotlin/fundamentals/Main.kt
@@ -23,7 +23,22 @@ fun main() {
         ),
     ))
 
+    cellar.describeBottles(MealType.WHITE_MEAT) { wineBottle, mealType ->
+        val winePairingAdvice = if (wineBottle.wineType.goesWellWith(
+                mealType
+            )
+        ) {
+            "Nous vous recommandons ce vin pour s'accorder avec votre plat."
+        } else {
+            "Ce vin n'est pas le meilleur choix pour accompagner votre plat."
+        }
 
+        "Vin ${buildWineTypeDescription(wineBottle.wineType)} de l'année ${wineBottle.year}. $winePairingAdvice"
+    }
+
+    cellar.describeBottles(MealType.RED_MEAT) { wineBottle, _ ->
+        "Vin ${buildWineTypeDescription(wineBottle.wineType)} de l'année ${wineBottle.year}."
+    }
 }
 
 fun buildWineTypeDescription(wineType: WineType): String =
