03af09c3c4093fc2f3147b81ee43165db2f1b665
Élise Rubio <elise.rubio@4sh.fr>
2025-03-03 09:40:08 +0100

    tp7 step 2 solution

--- tp7/src/main/kotlin/fmt/kotlin/fundamentals/Cellar.kt
+++ tp7/src/main/kotlin/fmt/kotlin/fundamentals/Cellar.kt
@@ -1,5 +1,9 @@
 package fmt.kotlin.fundamentals
 
 data class Cellar(val bottles: List<WineBottle>) {
-
+    fun describeBottles(descriptionBuilder: (WineBottle) -> String) {
+        for(i in bottles.indices) {
+            println("$i: ${descriptionBuilder(bottles[i])}")
+        }
+    }
 }
