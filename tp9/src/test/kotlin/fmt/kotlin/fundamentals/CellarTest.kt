dcd1b2e6985585ac5644288d44b1c0ce0981fa89
ctruchi <cyril.truchi@4sh.fr>
2025-03-04 14:13:22 +0400

    tp9 step 3

--- tp9/src/test/kotlin/fmt/kotlin/fundamentals/CellarTest.kt
+++ tp9/src/test/kotlin/fmt/kotlin/fundamentals/CellarTest.kt
@@ -1,43 +1,84 @@
 package fmt.kotlin.fundamentals
 
+import fmt.kotlin.fundamentals.UserRole.BUYER
+import fmt.kotlin.fundamentals.UserRole.OWNER
 import fmt.kotlin.fundamentals.WineColor.RED
 import fmt.kotlin.fundamentals.WineColor.WHITE
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
+import strikt.api.expectThrows
+import strikt.assertions.hasSize
 import strikt.assertions.isEqualTo
 
 class CellarTest {
 
-    @Test
-    fun `should get bottles description`() {
-        val description = Cellar(
-            listOf(
-                Bottle("Coucheroy", 2005, RED),
-                Bottle("Pedesclaux", 2009, RED)
-            )
-        ).describeBottles()
+    @Nested
+    inner class Description {
+        @Test
+        fun `should get bottles description`() {
+            val description = Cellar(
+                mutableListOf(
+                    Bottle("Coucheroy", 2005, RED),
+                    Bottle("Pedesclaux", 2009, RED)
+                )
+            ).describeBottles()
 
-        expectThat(description).isEqualTo("""
+            expectThat(description).isEqualTo(
+                """
             Bouteille de Coucheroy de 2005
             Bouteille de Pedesclaux de 2009
-        """.trimIndent())
-    }
-
-    @Test
-    fun `should get red bottles description`() {
-        val description = Cellar(
-            listOf(
-                Bottle("Coucheroy", 2005, RED),
-                Bottle("Camarsac", 2018, WHITE),
-                Bottle("Pedesclaux", 2009, RED),
+        """.trimIndent()
             )
-        ).describeRedBottles()
+        }
 
-        expectThat(description).isEqualTo("""
+        @Test
+        fun `should get red bottles description`() {
+            val description = Cellar(
+                mutableListOf(
+                    Bottle("Coucheroy", 2005, RED),
+                    Bottle("Camarsac", 2018, WHITE),
+                    Bottle("Pedesclaux", 2009, RED),
+                )
+            ).describeRedBottles()
+
+            expectThat(description).isEqualTo(
+                """
             Bouteille de Coucheroy de 2005
             Bouteille de blanc
             Bouteille de Pedesclaux de 2009
-        """.trimIndent())
+        """.trimIndent()
+            )
+        }
+    }
+
+    @Nested
+    inner class Role {
+        @Test
+        fun `should add bottle to cellar as owner`() {
+            Session.currentUser = owner
+
+            val cellar = Cellar(mutableListOf<Bottle>())
+
+            cellar.addBottle(Bottle("Coucheroy", 2005, RED))
+
+            expectThat(cellar.bottles).hasSize(1)
+        }
+
+        @Test
+        fun `should not add bottle to cellar as buyer`() {
+            Session.currentUser = buyer
+
+            val cellar = Cellar(mutableListOf<Bottle>())
+
+            expectThrows<UnauthorizedException> {
+                cellar.addBottle(Bottle("Coucheroy", 2005, RED))
+            }
+        }
+    }
+
+    companion object {
+        private val owner = User("bmagrez", OWNER)
+        private val buyer = User("pdupont", BUYER)
     }
 }
