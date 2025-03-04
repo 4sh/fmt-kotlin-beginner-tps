ead4ce1be06279107c3f07d697e7e606c966f0db
Élise Rubio <elise.rubio@4sh.fr>
2025-03-04 16:52:30 +0100

    tp3 step 1 solution

--- tp3/src/main/kotlin/fmt/kotlin/fundamentals/WineBarrel.kt
+++ tp3/src/main/kotlin/fmt/kotlin/fundamentals/WineBarrel.kt
@@ -1,2 +1,13 @@
 package fmt.kotlin.fundamentals
 
+class WineBarrel private constructor(
+    val capacityInLiters: Int = 225,
+    private var wineAgeInMonths: Int
+) {
+    constructor(capacityInLiters: Int = 225) : this(capacityInLiters, 0)
+
+    constructor(capacityInLiters: Int = 225, wineBarrel: WineBarrel) : this(
+        capacityInLiters,
+        wineBarrel.wineAgeInMonths
+    )
+}
